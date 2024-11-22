e5ae41b8bea641243f5db2712a19f91e20d89fc4
axxving <axxving@gmail.com>
2024-11-21 19:58:27 -0600

    feat: add project 06

--- proyecto06/main.js
+++ proyecto06/main.js
@@ -1,7 +1,3 @@
-const menuToggle = document.querySelector('.toggle');
-const navigation = document.querySelector('.navigation');
-
-menuToggle.onclick = () => {
-    menuToggle.classList.toggle('active');
-    navigation.classList.toggle('active');
+function imgURL(item){
+    document.querySelector(".imgBox img").src=item;
 }
